690797709d36fc51b08e3ee76213b7aa518e7118
angelopsilva74 <angelopsilva74@usp.br>
2025-11-03 13:28:04 -0300

    whitenoise reaplicado

--- game_reviews/settings.py
+++ game_reviews/settings.py
@@ -138,6 +138,7 @@ DEBUG = False
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
